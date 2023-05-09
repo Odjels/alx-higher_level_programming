8d1a41ec57bd6e99030b42625b96a1c39f9e2935
odegua osajele <odeguaj@gmail.com>
2023-05-09 12:36:50 -0700

    python hello world

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,7 +5,7 @@ def print_last_digit(number):
     if number > 0:
         number = number % 10
     else:
-        number = ((-1) * numbe)r % 10
+        number = ((-1) * number) % 10
 
     print("{:d}".format(number), end="")
     return number
