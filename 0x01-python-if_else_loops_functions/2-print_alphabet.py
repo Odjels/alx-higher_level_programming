abb4ae8245a38d18d5a883192d48b587433c759b
odegua osajele <odeguaj@gmail.com>
2023-05-09 08:16:19 -0700

    if/else

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+for a in range(97, 122):# using asky table for lower case
+        print("{:a}".format(a), end="")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,2 @@
+for n in range(0, 99):
+    print("{} = {}".format(n, hex(n)))
