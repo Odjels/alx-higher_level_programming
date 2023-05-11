d3e816a04f49566eca0abfe3b0584425fa6ee3c3
odegua osajele <odeguaj@gmail.com>
2023-05-11 11:01:19 -0700

    python import modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,9 +4,9 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
     if a < b:
-        x = add(a, b)
+        c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return (c)
-    else::
+    else:
         return(sub(a, b))
