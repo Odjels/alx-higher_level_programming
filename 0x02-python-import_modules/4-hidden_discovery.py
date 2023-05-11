7503208b1896409add7238cf33d12b3a2c6a4588
odegua osajele <odeguaj@gmail.com>
2023-05-11 09:41:25 -0700

    python import modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-
 if __name__ == "__main__":
     import hidden_4
     for names in dir(hidden_4):
         if names[:2] != "__":
-            print("{names:s}")
+            print("{:s}".format(names))
