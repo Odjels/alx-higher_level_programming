9811239193feaa30bdb35316cd2e16a26563720f
odegua osajele <odeguaj@gmail.com>
2023-05-17 10:32:00 -0700

    data structures

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    while value in a_dictionary.values():
-        for a, b in a_dictionary.items():
-            if b == value:
-                del a_dictionary[a]
-                break
-
-    return (a_dictionary)
+    keys_to_del = []
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            keys_to_del.append(key)
+    for key in keys_to_del:
+        del a_dictionary[key]
+    return a_dictionary
