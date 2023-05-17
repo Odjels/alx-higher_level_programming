5c19cc1c4b2aed8c63a40ef18e05743e84c6383e
odegua osajele <odeguaj@gmail.com>
2023-05-17 10:19:53 -0700

    data structures

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_k = sorted(a_dictionary.k())
-    for k in sorted_k:
-        print(f"{k:s}: {a_dictionary.get(k)}")
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        print(f"{key:s}: {a_dictionary.get(key)}")
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary.copy()
-    for key in new_dict.keys():
-        new_dict[key] = new_dict[key] * 2
-    return new_dict
+    return {key: bab * 2 for key, bab in a_dictionary.items()}
+
